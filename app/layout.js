228243985392ced92ef6d683652f230b072b48ab
kaundalvikas21 <vikakaundal21@gmail.com>
2026-01-28 10:11:22 +0530

    fix:the viewport to the device width and disable zooming

--- app/layout.js
+++ app/layout.js
@@ -30,6 +30,13 @@ export const metadata = {
   publisher: "VisaFly",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth" suppressHydrationWarning>
